2cb97297ea538eb95a4a8c8c35b07c75129974c6
Yichen Yan <oraluben@outlook.com>
2018-03-08 10:52:39 +0800

    init vue template

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data:  () => { return {
+    msg: "hello"
+  } }
+}
+</script>
